454073c0a1bd00b750b158c36cca70ffe8570c20
Paulo Ziembick <pauloziembick@gmail.com>
2024-08-29 21:15:58 -0300

    f commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.sass";
+
+export default function Home() {
+  return (
+    <h1 className={styles.main}>main page here</h1>
+  );
+}
